bf69216b84b4593e56d1e28bc119332aa6f1e93f
RazGavrieli <90526270+RazGavrieli@users.noreply.github.com>
2022-01-04 16:49:27 +0200

    navie algo for using less client.move

--- src/algo.py
+++ src/algo.py
@@ -1,3 +1,5 @@
+import copy
+import math
 import time
 
 from src.Graph.GraphAlgo import GraphAlgo
@@ -8,17 +10,18 @@ class gameAlgo:
     def __init__(self, g: GraphAlgo, client: Client):
         self.g = g
         self.client = client
+        self.ID = 0
+        self.lastdis = 0
+        self.lasttte = (int)(self.client.time_to_end())
+        self.i = 0
 
     def center_agents(self):
         agentnum = (int)(self.client.get_info().split(",")[8].split(":")[1].split("}")[0]) # microsoft call me for work 050-3331464
         # 0 is pokemons, 1 is 'is logged in', 2 is moves, 3 is grade, 4 is game level, 5 is max user level, 6 is id, 7 is graph, 8 is num of agents
-        print(self.client.get_info())
-        print(agentnum)
         tempgraph = GraphAlgo()
         tempgraph.load_from_json("serverGraph.json")
         for i in range(agentnum):
             center = str(tempgraph.centerPoint()[0])
-            print(center)
             self.client.add_agent("{\"id\":" + center + "}")
             tempgraph.get_graph().remove_node(tempgraph.centerPoint()[0])
 
@@ -29,32 +32,32 @@ class gameAlgo:
                 mindist = float('inf')
                 closeddestID = -1
                 closedtsrcID = -1
+                minpokeID = -1
                 for p in dictpoke:
                     srcID = p.get('Pokemon').get('srcID')
                     destID = p.get('Pokemon').get('destID')
                     agentPOS = a.get('src')
-
+                    pokeID = p.get('Pokemon').get('ID')
                     currdist = self.g.shortest_path(agentPOS, srcID)[0]
+
                     if currdist < mindist:
                         mindist = currdist
                         closedtsrcID = srcID
                         closeddestID = destID
+                        minpokeID = pokeID
 
                 if agentPOS != closedtsrcID:
                     next_node = self.g.shortest_path(agentPOS, closedtsrcID)[1][1]
-                    self.client.choose_next_edge(
-                        '{"agent_id":' + str(a.get('id')) + ', "next_node_id":' + str(next_node) + '}')
+                    self.client.choose_next_edge('{"agent_id":' + str(a.get('id')) + ', "next_node_id":' + str(next_node) + '}')
                 else:
                     next_node = closeddestID
-                    self.client.choose_next_edge(
-                        '{"agent_id":' + str(a.get('id')) + ', "next_node_id":' + str(next_node) + '}')
-                self.client.move()
-                # next_node = (a.get('src')+1)%self.g.get_graph().v_size()
-                # self.client.choose_next_edge('{"agent_id":'+str(a.get('id'))+', "next_node_id":'+str(next_node)+'}')
+                    self.client.choose_next_edge('{"agent_id":' + str(a.get('id')) + ', "next_node_id":' + str(next_node) + '}')
+
+
+
 
     def loadPoke(self, dictpoke: dict):
         epsilon = 0.00000001
-        #dictpoke = eval(self.client.get_pokemons()).get('Pokemons')
         for curr in dictpoke:
             p = curr.get('Pokemon')
             px, py, _ = eval(p.get('pos'))
@@ -79,9 +82,70 @@ class gameAlgo:
             p['srcID'] = minsrcID
             p['destID'] = mindestID
 
+
+
     def loadAgents(self, tempagents: list):
         agents = []
         for curr in tempagents:
             a = curr.get('Agent')
             agents.append(a)
         return agents
+
+    def move(self, agents, dictpoke, client):
+        s = False
+        ss = False
+        pokeflag = False
+        for a in agents:
+            if a.get('speed')>1: s = True
+            if a.get('speed')>4: ss = True
+
+        for a in agents:
+            src = a.get('src')
+            dest = a.get('dest')
+            pos = a.get('pos')
+            x, y, _ = eval(pos)
+
+            if dest != -1:
+                destx, desty, _ = self.g.get_graph().get_all_v().get(dest).pos
+                srcx, srcy, _ = self.g.get_graph().get_all_v().get(src).pos
+                weight = self.g.get_graph().all_out_edges_of_node(src).get(dest)
+
+                for curr in dictpoke:
+                    p = curr.get('Pokemon')
+                    if p.get('srcID') == src:
+                        destx, desty, _ = eval(p.get('pos'))
+                        pokeflag = True
+                currdist = math.sqrt((x - destx) ** 2 + (y - desty) ** 2)
+                if pokeflag:
+                    # edgelength = math.sqrt((srcx - destx) ** 2 + (srcy - desty) ** 2)
+                    # norm = currdist/edgelength
+                    # actual = weight*norm / a.get('speed')
+                    currtte = (int)(client.time_to_end())
+                    epsilon = 0.001
+                    if ((a.get('speed') ** 1.3) * weight * 0.00000251413213 * (self.lasttte - currtte)) > currdist - epsilon:
+                        self.lasttte = currtte
+                        self.client.move()
+                    self.lastdis = currdist
+        if not pokeflag:
+            if ss:
+                if self.i>=6:
+                    self.client.move()
+                    self.i=0
+                else:
+                    self.i+=1
+            elif s:
+                if self.i>=12:
+                    self.client.move()
+                    self.i=0
+                else:
+                    self.i+=1
+            else:
+                if self.i >= 20:
+                    self.client.move()
+                    self.i = 0
+                else:
+                    self.i += 1
+
+    def updateclient(self, client: Client):
+        self.client = client
+        self.lasttte = (int)(client.time_to_end())
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import json
+import time
 
 import pygame
 from pygame import *
 
+import math
 from gameGUI import gameGUI
 from algo import gameAlgo
 from src.Graph.GraphAlgo import GraphAlgo
@@ -10,7 +12,7 @@ from client_python.client import Client
 import subprocess
 
 # Auto server opener
-subprocess.Popen(["powershell.exe", "java -jar src/Ex4_Server_v0.0.jar 0"])
+subprocess.Popen(["powershell.exe", "java -jar Ex4_Server_v0.0.jar 14"])
 
 # Host information
 PORT = 6666
@@ -40,13 +42,26 @@ gameGUI = gameGUI(g, screen, client)
 gameAlgo = gameAlgo(g, client)
 gameAlgo.center_agents()
 client.start()
+gameAlgo.updateclient(client)
+gameGUI.updateclient(client)
+print(client.time_to_end())
+target = (int)(client.time_to_end())/100
 i = 0
 while client.is_running() == 'true':
     # check events
+    if (int)(client.time_to_end()) == 0:
+        client.stop_connection()
+        exit(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+        if event.type == MOUSEBUTTONDOWN:
+            if (0,60)<mouse.get_pos()<(150,80):
+                print("bye")
+                client.stop_connection()
+                exit(0)
+    clock.tick(60)
 
     """
     Following function calculates the src and dest nodes of each pokemon
@@ -72,13 +87,19 @@ while client.is_running() == 'true':
     "draw pokes"
     gameGUI.drawPokes(dictpoke)
 
-    print(agents, i)
-    #clock.tick(30)
     display.update()
-    # client.move()
 
+    gameAlgo.move(agents, dictpoke, client)
 
+    #print(client.time_to_end(), client.get_info())
+        # 0.000005984958324579959 is the progress for 1 tick of TTE on a 1.6449953 edge
+        # 0.000003989972216386639
+        # 0.000003989972217155122
+        # 0.000000794992533812333325
+        # 0.00000052999502254157365
+        # 0.00000119248880040237375
+        # 0.00000039749626690616665
+        # 0.0000002229342753096935
 
-
-
-
+print("OVER")
+print(client.is_running())
